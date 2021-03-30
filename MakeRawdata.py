a4d763b6fe15f009b3dd2205026663b32a7d8c62
ChungSeok Lee <lcs625@naver.com>
2021-03-30 17:55:29 +0900

    updates

--- MakeRawdata.py
+++ MakeRawdata.py
@@ -4,7 +4,13 @@ from matplotlib import pyplot as plt
 import datetime
 code_df = pd.read_csv('./kospi200_list.csv')
 
-start = datetime.datetime(2010,1,1)
-
+start = datetime.datetime(2021,2,1)
 df = pdr.get_data_yahoo("352820.KS", start)
-print(df.iloc[-1])
+
+print(df.iloc[30])
+print("#"*30)
+refer_day = 30
+timestamp = 15
+
+for idx in range(refer_day,len(df)):
+    print(df.iloc[idx])
