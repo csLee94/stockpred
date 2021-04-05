6e99bb2cc405f8298253c73ffaaeef5f74e9cd00
ChungSeok Lee <lcs625@naver.com>
2021-04-05 17:41:04 +0900

    updates

--- activate_model.py
+++ activate_model.py
@@ -10,6 +10,8 @@ import cv2
 import numpy as np
 import pandas as pd
 from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+import time
 
 refer_day =30
 timestamp=5
@@ -25,24 +27,21 @@ y_data = []
 path = "C:/Users/pc1/Documents/#0.LCS/stockpred/img/305/%s/%s"
 
 for label in category_list:
-    tlst = file_dict[label]
-    for infor in file_list[:10000]:
+    file_list = file_dict[label]
+    for infor in file_list:
         img = cv2.imread(path % (label, infor))
         x_data.append(img/255)
         y_data.append(label)
-    time.sleep(1)
+    # time.sleep(1)
 
 np_x_data = np.array(x_data)
 np_y_data = np.array(y_data)
 np_y_encoding = np_utils.to_categorical(np_y_data)
 
-np_x_train = np_x_data[:9000]
-np_y_train = np_y_encoding[:9000]
-np_x_test = np_x_data[-1000:]
-np_y_test = np_y_encoding[-1000:]
+np_x_train, np_x_test, np_y_train, np_y_test = train_test_split(np_x_data, np_y_data, test_size=0.2)
 
 model = Sequential()
-model.add(Conv2D(12, kernel_size=(5,5), activation='relu', input_shape=(240,320, 3), strides=(2,2), padding='same'))
+model.add(Conv2D(12, kernel_size=(5,5), activation='relu', input_shape=(120,160, 3), strides=(2,2), padding='same'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Conv2D(16, kernel_size=(5,5), activation='relu', strides=(2,2),padding='same'))
 model.add(MaxPooling2D(pool_size=(2,2)))
@@ -56,29 +55,31 @@ model.add(Dense(60, activation='relu'))
 model.add(Dense(24, activation='relu'))
 model.add(Dense(12, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(np_x_data_train, np_y_train, batch=25, epochs = 1000)
+model.fit(np_x_train, np_y_train, batch_size=25, epochs = 1000)
 
 pred = model.predict_classes(np_x_test)
 
 real_dict ={
-    "[0. 1. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]":0,
-    "[0. 0. 1. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]":1,
-    "[0. 0. 0. 1. 0. 0. 0. 0. 0. 0. 0. 0. 0.]":2,
-    "[0. 0. 0. 0. 1. 0. 0. 0. 0. 0. 0. 0. 0.]":3,
-    "[0. 0. 0. 0. 0. 1. 0. 0. 0. 0. 0. 0. 0.]":4,
-    "[0. 0. 0. 0. 0. 0. 1. 0. 0. 0. 0. 0. 0.]":5,
-    "[0. 0. 0. 0. 0. 0. 0. 1. 0. 0. 0. 0. 0.]":6,
-    "[0. 0. 0. 0. 0. 0. 0. 0. 1. 0. 0. 0. 0.]":7,
-    "[0. 0. 0. 0. 0. 0. 0. 0. 0. 1. 0. 0. 0.]":8,
-    "[0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 1. 0. 0.]":9,
-    "[0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 1. 0.]":10,
-    "[0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 1.]":11,
+    "[1. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]":0,
+    "[0. 1. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]":1,
+    "[0. 0. 1. 0. 0. 0. 0. 0. 0. 0. 0. 0.]":2,
+    "[0. 0. 0. 1. 0. 0. 0. 0. 0. 0. 0. 0.]":3,
+    "[0. 0. 0. 0. 1. 0. 0. 0. 0. 0. 0. 0.]":4,
+    "[0. 0. 0. 0. 0. 1. 0. 0. 0. 0. 0. 0.]":5,
+    "[0. 0. 0. 0. 0. 0. 1. 0. 0. 0. 0. 0.]":6,
+    "[0. 0. 0. 0. 0. 0. 0. 1. 0. 0. 0. 0.]":7,
+    "[0. 0. 0. 0. 0. 0. 0. 0. 1. 0. 0. 0.]":8,
+    "[0. 0. 0. 0. 0. 0. 0. 0. 0. 1. 0. 0.]":9,
+    "[0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 1. 0.]":10,
+    "[0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 1.]":11,
 
 }
+
 real_lst =[]
-for infor in np_y_data_test:
-  real_lst.append(real_dict[str(infor)])
+for infor in np_y_test:
+    # print(infor)
+    real_lst.append(real_dict[str(infor)])
 
 real_np = np.array(real_lst)
-result_df  = pd.DataFrame(coufusion_matrix(real_np, pred))
+result_df  = pd.DataFrame(confusion_matrix(real_np, pred))
 result_df.to_excel('./result.xlsx')
--- temp.py
+++ temp.py
@@ -1,22 +1,28 @@
-import pandas as pd
-import pandas_datareader as pdr
-from MakeRawdata import makerawdata
-import time
+# import pandas as pd
+# import pandas_datareader as pdr
+# from MakeRawdata import makerawdata
+# import time
 
-code_data = pd.read_csv("./kospi_list.csv", encoding='cp949')
-print(list(code_data['code'][:3]))
+# code_data = pd.read_csv("./kospi_list.csv", encoding='cp949')
+# print(list(code_data['code'][:3]))
 
-refer_day = 30
-timestamp = 5
+# refer_day = 30
+# timestamp = 5
 
 
-for code in list(code_data['code'][:3]):
-    s_code = code[3:-3]
-    makerawdata(s_code, refer_day, timestamp)
-    name = str(code_data.loc[code_data['code']==code]['name'].values).split("'")[1]
-    time_tuple = time.localtime()
-    time_str = time.strftime("%m/%d/%Y, %H:%M:%S", time_tuple).split(", ")[0]
-    memo = open('memo.txt', 'a', encoding='utf8')
-    memo.write(str(name)+"   "+time_str+'\n')
-    memo.close()
-    print("Done: %s" % str(name))
+# for code in list(code_data['code'][:3]):
+#     s_code = code[3:-3]
+#     makerawdata(s_code, refer_day, timestamp)
+#     name = str(code_data.loc[code_data['code']==code]['name'].values).split("'")[1]
+#     time_tuple = time.localtime()
+#     time_str = time.strftime("%m/%d/%Y, %H:%M:%S", time_tuple).split(", ")[0]
+#     memo = open('memo.txt', 'a', encoding='utf8')
+#     memo.write(str(name)+"   "+time_str+'\n')
+#     memo.close()
+#     print("Done: %s" % str(name))
+
+import tensorflow
+import keras
+print(tensorflow.__version__)
+print(keras.__version__)
+# 
